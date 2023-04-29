9d84d0e01271e1f4047729716dd7d967ef809add
Joshua Salva <joshuarigs.salva@lc.cuny.edu>
2023-04-28 20:35:00 -0400

    modified package.json and index.js by following the tutorial

--- server/index.js
+++ server/index.js
@@ -7,11 +7,30 @@ const path = require('path')
 // define a PORT for our server to run
 const PORT = 8000
 
+
+//new code from tutorial for postgres
+const bodyParser = require('body-parser')
+
 // initialize an express application
 const app = express()
 
 //host react app as static files
 app.use(express.static(path.resolve(__dirname, '../favlinks/build')))
+
+
+//new code from tutorial for postgres
+app.use(bodyParser.json())
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+)
+
+//new code from tutorial for postgres
+app.get('/', (request, response) => {
+    response.json({ info: 'Node.js, Express, and Postgres API' })
+})
+
 
 // define some routes
 app.get('/', (request, response) =>{
@@ -19,6 +38,12 @@ app.get('/', (request, response) =>{
     response.sendFile(path.resolve(__dirname, '../favlinks/build',
      'index.html'))
 })
+
+//new code from tutorial for postgres
+app.listen(port, () => {
+    console.log(`App running on port ${port}.`)
+})
+
 
 // start app at PORT
 app.listen(PORT, () => {
